25b626ca46d9d548ce5d110aa0ed89e77d1a3a8d
christophoruswaldorfschool <158027169+christophoruswaldorfschool@users.noreply.github.com>
2024-04-25 10:33:17 +0000

    podium page 4th attempt

---
+++ src/pages/podium.js
@@ -0,0 +1,79 @@
+import Content from '../components/content'
+import ContentBox from '../components/content-box'
+import ContentList from '../components/contentlist'
+import Hero from '../components/hero'
+import Layout from '../components/layout'
+import SectionTitle from '../components/section-title'
+import Seo from '../components/seo'
+import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
+import { graphql } from 'gatsby'
+import React from 'react'
+import truncate from 'truncate'
+
+const PodiumPageTemplate = ({ data }) => {
+  const {
+    page: {
+      title,
+      lead: { lead },
+      relatedContentTitle,
+    },
+    allContentfulPost: { posts },
+  } = data
+
+  return (
+    <Layout menu="posts">
+      <Seo title={title} description={lead} />
+      <Hero title={title} lead={lead} color="gold" />
+      <Content>
+        <SectionTitle title={relatedContentTitle} align="right" color="gold" />
+        <ContentList type="full">
+          {posts
+            .filter((item) => !!item.content)
+            .map((item) => (
+              <ContentBox
+                title={item.title}
+                type="full"
+                color="gold"
+                buttonText="Tovább"
+                buttonLink={`/eloadasok/${item.slug}`}
+                key={item.slug}
+                image={item.image}
+              >
+                {truncate(
+                  documentToPlainTextString(JSON.parse(item.content.raw)),
+                  740
+                )}
+              </ContentBox>
+            ))}
+        </ContentList>
+      </Content>
+    </Layout>
+  )
+}
+
+export default PodiumPageTemplate
+
+export const pageQuery = graphql`
+  query EloadasPageQuery {
+    page: contentfulPage(slug: { eq: "eloadasok" }) {
+      lead {
+        lead
+      }
+      title
+      relatedContentTitle
+    }
+    allContentfulEloadas(sort: { fields: date, order: DESC }) {
+      posts: nodes {
+        date
+        slug
+        title
+        content {
+          raw
+        }
+        image: eloadasPicture {
+          gatsbyImage(width: 488, placeholder: BLURRED, cropFocus: CENTER)
+        }
+      }
+    }
+  }
+`
